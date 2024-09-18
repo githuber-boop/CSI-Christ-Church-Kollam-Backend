4483b1e7ba136e5cb31dbc10ad1e6a1571a61f9c
Ashish <ashishalex084@gmail.com>
2024-09-18 14:10:46 +0530

    cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true })); 
 app.use('/api/users',userRouter)
 app.use('/api/message',messageRouter)
